42e94ca2817b3b2deca00a3654a5561ca5b8e9b3
DevShankarani64 <77530987+DevShankarani64@users.noreply.github.com>
2021-07-17 13:37:13 +0530

    Add files via upload

---
+++ maze.js
@@ -0,0 +1,51 @@
+function maze(){
+//var sophia= createSprite(20,25,18,18);
+//sophia.shapeColor="Red";
+var cup= createSprite(380,364,20,20);
+cup.shapeColor="Yellow";
+var metal= createSprite(10,70,100,20);
+metal.shapeColor="Red";
+var metal1= createSprite(104,50,20,120);
+metal1.shapeColor="Red";
+var metal2= createSprite(145,40,100,20);
+metal2.shapeColor="Red";
+var metal3= createSprite(72,166,100,20);
+metal3.shapeColor="Red";
+var metal4= createSprite(180,192,20,110);
+metal4.shapeColor="Red";
+var metal5= createSprite(212,134,100,20);
+metal5.shapeColor="Red";
+var metal6= createSprite(276,36,20,80);
+metal6.shapeColor="Red";
+var metal7= createSprite(362,86,20,85);
+metal7.shapeColor="Red";
+var metal8= createSprite(312,176,90,20);
+metal8.shapeColor="Red";
+var metal9= createSprite(72,226,20,120);
+metal9.shapeColor="Red";
+var metal10= createSprite(248,228,70,20);
+metal10.shapeColor="Red";
+var metal11= createSprite(108,318,120,20);
+metal11.shapeColor="Red";
+var metal12= createSprite(198,364,20,90);
+metal12.shapeColor="Red";
+var metal13= createSprite(340,218,20,90);
+metal13.shapeColor="Red";
+var metal14= createSprite(264,292,20,90);
+metal14.shapeColor="Red";
+var metal15= createSprite(316,348,20,90);
+metal15.shapeColor="Grey";
+var metal16= createSprite(38,380,120,20);
+metal16.shapeColor="Red";
+var metal17= createSprite(106,358,20,60);
+metal17.shapeColor="Red";
+var metal18= createSprite(334,39,120,20);
+metal18.shapeColor="Red";
+var metal19= createSprite(273,198,20,60);
+metal19.shapeColor="Red";
+
+ 
+  createEdgeSprites();
+}
+
+ 
